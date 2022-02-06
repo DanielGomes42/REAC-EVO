330b12c4646875a2b93b7f42a72ee9fb90a0dfde
DanilGomes42 <daniel757@gmail.com>
2022-02-06 20:26:27 -0300

    up

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Text } from 'react-native'
 
 
-function App(){
-  return  <Text> Primeiro Componente</Text>
- }
 
 
- export default App
+  
+  export default () => <Text> Primeiro Componente</Text>
+ 
+
+
+ 
