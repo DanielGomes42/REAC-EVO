9d663d14709802156c54d286f3a68c6434fd8223
DanilGomes42 <daniel757@gmail.com>
2022-02-09 17:52:38 -0300

    add segundo

---
+++ src/componentes/segundo.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {View , Text} from 'react-native'
+
+import   Primeiro  from './componentes/Primeiro'
+
+export default () => (<View><primeiro>Fazendo um teste para o Git</primeiro></View>)
